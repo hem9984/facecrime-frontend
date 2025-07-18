7df868b00634f22ce8bbfbe1f457782357fe0d6d
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2025-07-18 15:04:44 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { PostHogProvider } from 'posthog-js/react';
+import posthog from 'posthog-js';
 
+const apiKey = process.env.REACT_APP_POSTHOG_KEY;
+console.log("PostHog API Key:", apiKey);
 const options = {
-  api_host: process.env.REACT_APP_POSTHOG_HOST, // For CRA, use process.env.REACT_APP_*
-  // Add any other options you need
+  api_host: process.env.REACT_APP_POSTHOG_HOST || "",
 };
+
+if (apiKey) {
+  posthog.init(apiKey, options);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PostHogProvider apiKey={process.env.REACT_APP_POSTHOG_KEY!} options={options}>
+    <PostHogProvider apiKey={apiKey || ""} options={options}>
       <App />
     </PostHogProvider>
   </React.StrictMode>
